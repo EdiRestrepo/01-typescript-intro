19dc7bbfdab450f57098a524965caab403990ca3
EdiRestrepo <eerestre@bancolombia.com.co>
2023-11-22 22:14:28 -0500

    01-basic-types

---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,13 @@
+const name: string = 'Strider';
+
+let hpPoints: number | 'FULL' = 95;
+
+const isAlive: boolean = true;
+
+hpPoints = 'FULL';
+
+console.log({
+    name, hpPoints, isAlive
+})
+
+export {}; //Esta linea de codigo significa que este archivo es un modulo
